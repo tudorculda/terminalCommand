d7d2cdbe271efa3d940ae5cf5de54d1d13340bb4
Unknown <tudor.culda@gmail.com>
2021-10-09 21:53:18 +0300

    working version 1

--- src/main/java/com/harulmures/runcommand/controller/CommandCOntroller.java
+++ src/main/java/com/harulmures/runcommand/controller/CommandCOntroller.java
@@ -1,27 +1,30 @@
 package com.harulmures.runcommand.controller;
 
 import com.harulmures.runcommand.service.CommandService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("run")
+@Slf4j
 public class CommandCOntroller {
     @Autowired
     private CommandService commandService;
 
-    @GetMapping("vlc")
+    @GetMapping("video")
     public ResponseEntity<String> runWithParameters(@RequestParam("file") String file){
         try{
 
-        commandService.runVlc(file);
+        commandService.runVideo(file);
         return new ResponseEntity<>("Comanda a fost lansata", HttpStatus.OK);
         } catch(Exception ex){
+            log.error(ex.getMessage(), ex);
+
             return new ResponseEntity<>("Ceva nu a mers bine, vorbeste cu Tudor, noteaza timpul la care a aparut acest mesaj (" + LocalDateTime.now().toString() + "):" + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
--- src/main/java/com/harulmures/runcommand/service/CommandService.java
+++ src/main/java/com/harulmures/runcommand/service/CommandService.java
@@ -6,28 +6,51 @@ import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @Slf4j
 public class CommandService {
 
-    @Value("${vlc.command}")
+    @Value("${video.command}")
     private String vlcCommand;
 
-    @Value("${vlc.close-previous}")
-    private Boolean cleanVlcBeforeStart;
+    @Value("${video.close-previous}")
+    private Boolean cleanVideoBeforeStart;
 
-    @Value("${vlc.video-folder:.}")
-    private String vlcFolder;
+    @Value("${video.video-folder:.}")
+    private String videoFolder;
 
-    private List<String> openedVlcPids;
+    private List<Process> openedVideoProcesses = new ArrayList<>();
 
-    public void runVlc(String file) throws FileNotFoundException {
+    public void runVideo(String fileName) throws IOException {
+        if (cleanVideoBeforeStart) {
+            terminatePreviousProcesses();
+        }
 
-        String command = MessageFormat.format(vlcCommand, vlcFolder + File.separator + file);
-        log.info("Will run the following command: {}", command);
-        throw new IllegalArgumentException("nu am gasit fisierul");
+        File file = new File(videoFolder, fileName);
+        if (!file.exists()) {
+            throw new FileNotFoundException(MessageFormat.format("Fisierul {0} nu a fost gasit in folderul {1}", fileName, videoFolder));
+        }
+        String command = MessageFormat.format(vlcCommand, file.getAbsolutePath());
+        log.info("Se pregateste pentru executie comanda={}", command);
+        Process process = Runtime.getRuntime().exec(command);
+        log.info("Am lansat comanda:{}    --> process id:{}", command, process.pid());
+        openedVideoProcesses.add(process);
+
+
+    }
+
+    private void terminatePreviousProcesses() {
+        openedVideoProcesses.forEach(this::killProcess);
+        openedVideoProcesses.clear();
+    }
+
+    private void killProcess(Process prc) {
+        log.info("Se pregateste pentru terminare procesul={}", prc.info().toString());
+        prc.destroy();
     }
 }
